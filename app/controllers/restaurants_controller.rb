b459884b2a199212b6ffbf33472a57e6a5c3754f
Sophie Meier <mesophie@gmx.ch>
2017-07-05 13:54:06 -0400

    backend done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,56 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+   @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    respond_to do |format|
+      format.html { redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @restaurant.update(restaurant_params)
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully updated.' }
+        format.json { render :show, status: :ok, location: @restaurant }
+      else
+        format.html { render :edit }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  private
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    def restaurant_params
+    params.required(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
